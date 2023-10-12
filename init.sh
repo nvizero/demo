d40a33f00817a8dc35e900bd46b4626432f35475
victor.tseng <withvictor@gmail.com>
2023-10-12 11:08:26 +0800

    fix sh

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@
 /usr/bin/php /var/www/artisan db:seed --class=PermissionTableSeeder
 /usr/bin/php /var/www/artisan db:seed --class=CreateAdminUserSeeder
 /usr/bin/php /var/www/artisan config:cache
+rm -rf /var/www/public/storage
 /usr/bin/php /var/www/artisan storage:link
--- mkd.sh
+++ mkd.sh
@@ -4,3 +4,5 @@
 mkdir -p /var/www/storage/framework/sessions
 mkdir -p /var/www/storage/framework/cache
 mkdir -p /var/www/storage/framework/views
+chmod -R 777 /var/www/storage
+chmod -R 777 /var/www/bootstrap
