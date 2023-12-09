0540f092dbd66b407ee48e1b86063433c49eb894
victor.tseng <withvictor@gmail.com>
2023-12-09 21:46:53 +0800

    fix title

--- resources/views/layouts/backend_partials/sidemenu.blade.php
+++ resources/views/layouts/backend_partials/sidemenu.blade.php
@@ -10,7 +10,7 @@
                     <li><a href="{{ route('prodcatelist') }}">產品分類</a></li>
                     <li><a href="{{ route('abouts.index') }}">關於我們</a></li>
                     <li><a href="{{ route('index_about.index') }}">首頁-關於我們</a></li>
-                    <li><a href="{{ route('index_show.index') }}">首頁-顯示</a></li>
+                    <li><a href="{{ route('index_show.index') }}">首頁-橫幅標題(上方)</a></li>
                     <li><a href="{{ route('keyval.index') }}">公司資訊</a></li>
 
                 <li class="menu-title">帳號設定 </li>
